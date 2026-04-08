6bfc8e48eb33587eadfa9368e182630471b203b3
Prakhar Chaturvedi <prakharjwell2210096@akgec.ac.in>
2026-04-09 04:13:52 +0530

    fix multi-task env for validator

--- env/main_env.py
+++ env/main_env.py
@@ -1,10 +1,10 @@
-
 import json
 from tasks.graders import email_grade, meeting_grade, doc_grade
 
 class OfficeEnv:
     def __init__(self):
-        self.task_id = 0
+        self.tasks = ["email", "meeting", "doc"]
+        self.current_task = 0
         self.done = False
 
         self.email_data = json.load(open("data/emails.json"))
@@ -12,32 +12,49 @@ class OfficeEnv:
         self.doc_data = json.load(open("data/docs.json"))
 
     async def reset(self):
-        self.task_id = 0
+        self.current_task = 0
         self.done = False
+
         return type("Res", (), {
-            "observation": {"task": "email", "content": self.email_data[0]["text"]},
+            "observation": {
+                "task": "email",
+                "content": self.email_data[0]["text"]
+            },
             "done": False
         })
 
     async def step(self, action):
-        if self.task_id == 0:
+        task = self.tasks[self.current_task]
+
+        if task == "email":
             expected = self.email_data[0]["label"]
             reward = email_grade(action, expected)
-            obs = {"task": "meeting", "content": self.meeting_data[0]["request"]}
-        elif self.task_id == 1:
+            next_obs = {
+                "task": "meeting",
+                "content": self.meeting_data[0]["request"]
+            }
+
+        elif task == "meeting":
             expected = self.meeting_data[0]["valid"]
             reward = meeting_grade(action, expected)
-            obs = {"task": "doc", "content": self.doc_data[0]["text"]}
-        else:
+            next_obs = {
+                "task": "doc",
+                "content": self.doc_data[0]["text"]
+            }
+
+        else:  # doc
             expected = self.doc_data[0]["risk"]
             reward = doc_grade(action, expected)
             self.done = True
-            obs = {"task": "done", "content": "completed"}
+            next_obs = {
+                "task": "done",
+                "content": "completed"
+            }
 
-        self.task_id += 1
+        self.current_task += 1
 
         return type("Res", (), {
-            "observation": obs,
+            "observation": next_obs,
             "reward": reward,
             "done": self.done
         })
